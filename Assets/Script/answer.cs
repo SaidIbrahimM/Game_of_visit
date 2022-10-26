b7a2480026089bc713154bd5a41c37306f5c1540
fabieng06 <fabien.gagey@gmail.com>
2022-10-26 15:59:56 +0200

    Revert "Exemple of quiz with mouse (not oculus)"
    This reverts commit 895814689cf74459ea9e24161390d0b352eee535.

--- Assets/Script/answer.cs
+++ Assets/Script/answer.cs
@@ -6,25 +6,18 @@ public class answer : MonoBehaviour
 {
     void OnMouseDown ()
     {
-        if(GameObject.Find("Canvas").GetComponent<quiz>().answer == transform.GetChild(0).GetComponent<TextMesh>().text)
-        {
-            Debug.Log("Win");
-        }
-        else
-        {
-            Debug.Log("Lose");
-        }
+        
     }
 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
+    // Start is called before the first frame update
+    void Start()
+    {
         
-    // }
+    }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
+    // Update is called once per frame
+    void Update()
+    {
         
-    // }
+    }
 }
