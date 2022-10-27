30acb4ccd1fb5a8c066df360441b5cd02d656eaf
Said.Ibrahim <is.said.ibrahim@gmail.com>
2022-10-27 08:49:30 +0200

    POC

--- Assets/Script/answer.cs
+++ Assets/Script/answer.cs
@@ -1,30 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class answer : MonoBehaviour
 {
+    private Text Ans;
+
     void OnMouseDown ()
     {
         if(GameObject.Find("Canvas").GetComponent<quiz>().answer == transform.GetChild(0).GetComponent<TextMesh>().text)
         {
-            Debug.Log("Win");
+            Debug.Log("Win !");
+            Ans.text = "Win !";
         }
         else
         {
-            Debug.Log("Lose");
+            Debug.Log("Lose !");
+            Ans.text = "Lose !";
         }
     }
 
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Ans=GameObject.Find("Ans").GetComponent<Text>();
+    }
 
-    // // Update is called once per frame
-    // void Update()
-    // {
+    // Update is called once per frame
+    void Update()
+    {
         
-    // }
+    }
 }
--- Assets/Script/quiz.cs
+++ Assets/Script/quiz.cs
@@ -8,26 +8,32 @@ using UnityEngine.UI;
 public class quiz : MonoBehaviour
 {
     private Text TxtQuestion;
+    // private Text RespL;
+    // private Text RespR;
     private TextMesh RespL;
     private TextMesh RespR;
     private int nbrand;
     public string answer;
 
     //Déclaration tableau
-    string[] quizz = new string[3];
+    string[] quizz = new string[5];
 
     // Start is called before the first frame update
     void Start()
     {
         //Component
         TxtQuestion=GameObject.Find("TxtQuestion").GetComponent<Text>();
-        RespL = GameObject.Find("RespL").GetComponent<TextMesh>();
-        RespR = GameObject.Find("RespR").GetComponent<TextMesh>();
+        //RespL = GameObject.Find("btn1").GetComponent<Text>();
+        //RespR = GameObject.Find("btn2").GetComponent<Text>();
+        RespL = GameObject.Find("R1").GetComponent<TextMesh>();
+        RespR = GameObject.Find("R2").GetComponent<TextMesh>();
 
         //Declaration du contenu du tableau
-        quizz[0]= "question 1 ?,R1,R2,R1";
-        quizz[1]= "question 2 ?,R11,R12,R12";
-        quizz[2]= "question 3 ?,R21,R222,R21";
+        quizz[0]= "Le cube immersif est conçu pour fonctionner avec la VR,Vrai,Faux,Faux";
+        quizz[1]= "Le cube doit être un espace clôt,Vrai,Faux,Faux";
+        quizz[2]= "Le cube immersif permet de scénariser l’activité,Vrai,Faux,Vrai";
+        quizz[3]= "La réalité virtuelle est identique à la vidéo 360 ?,Vrai,Faux,Faux";
+        quizz[4]= "La réalité virtuelle et la réalité augmentée sont des techniques identiques,Vrai,Faux,Faux";
 
         Ask();
         
